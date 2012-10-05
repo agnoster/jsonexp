4c17460d83bac535c78346b48ac2467ee26d1222
Isaac Wolkerstorfer <i@agnoster.net>
2012-10-05 23:21:42 +0200

    Remove hooks feature (temporarily)

--- test/test.js
+++ test/test.js
@@ -1,7 +1,6 @@
 var JSONExp = require('../lib/')
   , vows = require('vows')
   , assert = require('assert')
-  , sugar = require('sugar')
 
 function test(src, cases) {
     var batch = {}
@@ -118,9 +117,5 @@ vows.describe('JSONExp')
         , '[5, 4, 3, 2, 1, 0]': {}
         , '[1, 0, 2]': null
         }
-))
-.addBatch(testSame(
-    '{ "foo": <date: 1 month ago> }',
-    '{ "foo": "' + Date.create('1 month ago').format('{yyyy}-{MM}-{dd}T{hh}:{mm}:{ss}{zzzz}') + '" }'
 ))
 .export(module)
